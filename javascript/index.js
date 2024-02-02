d69fc0393e4d1249e196718f22ea2ca737401c27
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-02-02 15:41:37 -0300

    Adicionei botões para trocar os menus *Finalizado*

--- javascript/index.js
+++ javascript/index.js
@@ -16,9 +16,6 @@ btn_mostrarMenos.addEventListener('click',(evt)=>{
 })
 
 //Botões menu:
-
-const botoes_menu = [...document.querySelectorAll('.botao-menu')]
-
 let btn_pizza = document.querySelector('#btn_pizza')
 let btn_hamburguer = document.querySelector('#btn_hamburguer')
 let btn_bebidas = document.querySelector('#btn_bebidas')
@@ -27,3 +24,44 @@ let menu_pizza = [...document.querySelectorAll('#menu_pizza')]
 let menu_hamburguer = [...document.querySelectorAll('#menu_hamburguer')]
 let menu_bebidas = [...document.querySelectorAll('#menu_bebidas')]
 
+btn_pizza.addEventListener('click',(evt)=>{
+    menu_pizza.map((el)=>{
+        el.classList.add('adicionar')
+    })
+
+    menu_hamburguer.map((el)=>{
+        el.classList.remove('adicionar')
+    })
+
+    menu_bebidas.map((el)=>{
+        el.classList.remove('adicionar')
+    })
+})
+
+btn_hamburguer.addEventListener('click',(evt)=>{
+    menu_pizza.map((el)=>{
+        el.classList.remove('adicionar')
+    })
+
+    menu_hamburguer.map((el)=>{
+        el.classList.add('adicionar')
+    })
+
+    menu_bebidas.map((el)=>{
+        el.classList.remove('adicionar')
+    })
+})
+
+btn_bebidas.addEventListener('click',(evt)=>{
+    menu_pizza.map((el)=>{
+        el.classList.remove('adicionar')
+    })
+
+    menu_hamburguer.map((el)=>{
+        el.classList.remove('adicionar')
+    })
+
+    menu_bebidas.map((el)=>{
+        el.classList.add('adicionar')
+    })
+})
